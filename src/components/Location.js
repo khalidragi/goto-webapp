443f64c5deec19420f0bf62c628d8eab9d28f115
Khalid Ragi <khalidragi@Khalids-MacBook-Pro.local>
2019-10-05 17:04:40 +0300

    export api call as seperate function

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
-import axios from 'axios';
 import { ReasturantsContext } from '../context/ReasturantsContext';
-import { getCurrentPosition } from '../utils/utils';
+import { getCurrentPosition, getReasts } from '../utils/utils';
 
 const Location = () => {
   const { setReasturants, setFindMe, setLoading, isLoading } = useContext(
@@ -12,20 +11,7 @@ const Location = () => {
     setLoading(true);
     const { coords } = await getCurrentPosition();
     const { latitude, longitude } = coords;
-    let res = await axios.get(
-      `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}`,
-      {
-        //required authorization format from API
-        headers: {
-          //to get the API from the .env file use process.env.{variable name}
-          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
-        },
-        //option params passed to API call to retrieve only breakfast and lunch spots
-        params: {
-          categories: 'reasturant'
-        }
-      }
-    );
+    let res = await getReasts(latitude, longitude);
     let list = await res.data.businesses;
     setReasturants(list);
     console.log(list);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,26 @@
+import axios from 'axios';
+
 export function getCurrentPosition(options = {}) {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject, options);
   });
 }
+
+export const getReasts = async (latitude, longitude) => {
+  let res = await axios.get(
+    `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}`,
+    {
+      //required authorization format from API
+      headers: {
+        //to get the API from the .env file use process.env.{variable name}
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+      },
+      //option params passed to API call to retrieve only breakfast and lunch spots
+      params: {
+        categories: 'reasturant'
+      }
+    }
+  );
+
+  return res;
+};
